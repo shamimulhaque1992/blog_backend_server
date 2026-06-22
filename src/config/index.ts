41cc338558803296601a5a39c53ce52698857c45
shamimulhaquewebsure <shamimul@websure.digital>
2026-06-22 18:02:49 +0600

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({
+  path: path.join(process.cwd(), ".env"),
+});
+
+export default {
+  port: process.env.PORT || 8000,
+  app_url: process.env.APP_URL || "http://localhost:8000",
+  database_url: process.env.DATABASE_URL || "",
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_token_secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+  jwt_refresh_token_secret: process.env.JWT_REFRESH_TOKEN_SECRET,
+  jwt_access_token_expiry: process.env.JWT_ACCESS_TOKEN_EXPIRY,
+  jwt_refresh_token_expiry: process.env.JWT_REFRESH_TOKEN_EXPIRY,
+};
